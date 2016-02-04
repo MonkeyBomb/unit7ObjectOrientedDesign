7f38f4c1bd282fb832f644f89a9e63abf3467043
Karl <kdirzys@NN123-HA-10.HS.sd203.org>
2016-02-04 10:29:43 -0600

    CHapter 10 stuff

--- Chapter 8 practice/BillFoldTester.java
+++ Chapter 8 practice/BillFoldTester.java
@@ -16,4 +16,5 @@ public class BillFoldTester
    public static void main(String[] args)
    {
        
+   }
 }
--- Chapter 8 practice/IDCard.java
+++ Chapter 8 practice/IDCard.java
@@ -2,12 +2,27 @@
 
 public class IDCard extends CardHierarchy
 {
-    String idNumber;
+    private String idNumber;
     
     public IDCard(String name, String id)
     {
         super(name);
         idNumber = id;
+    }
+    
+    public boolean equals( Object other )
+    {
+        //check if this object and the other object are both instances of the smae class
+        if( this.getClass() == other.getClass())
+        {
+            // since both objects are instances of IDCard, it is okay to cast othjer to an IDCard reference
+            IDCard otherIDCard = (IDCard) other;
+            
+            boolean isEqual = super.equals(otherIDCard);
+            return isEqual && idNumber.equals( otherIDCard.idNumber );
+        }
+        
+        return false;
     }
     
     public String format()
